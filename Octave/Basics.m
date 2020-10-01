b51c75ddbadafe7313e5da1189aca28cc3b20c6d
PaareshC <paareshchaudhari@gmail.com>
2020-10-01 15:15:03 +0530

    Octave Basics Done

---
+++ Octave/Basics.m
@@ -0,0 +1,50 @@
+% comment
+5+6 
+3-2
+5*8
+1/2
+2^6
+1==2 % false
+1~=2 % true
+1&&0 % AND
+1||0 % OR
+xor(1,0)
+a=3
+a=3; % semicolon supressing output
+b="hi"
+c=(3>=1)
+a=pi;
+a
+disp(a)
+disp(sprintf('2 decimals: %0.2f',a))
+disp(sprintf('6 decimals: %0.6f',a))
+a
+format long
+a
+format short
+a
+A=[1 2; 3 4; 5 6]
+A=[1 2;
+	3 4;
+	5 6]
+v=[1 2 3]
+v=[1 ; 3; 2]
+v=1:0.1:2
+v=1:6
+ones(2,3)
+c=2*ones(2,3)
+c=[2 2 2;2 2 2]
+w=ones(1,3)
+w=zeros(1,3)
+w=rand(1,3)
+rand(3,3)
+rand(3,3)
+w=randn(1,3)
+w=randn(1,3)
+w=-6+sqrt(10)*(randn(1,100))
+hist(w)
+hist(w,5)
+eye(4)
+I=eye(6)
+I
+help eye % to read documentation
